db57bd7faea002d5e73e0468a6e131416c1059df
Faison Zutavern <fzutavern@my.wctc.edu>
2017-07-12 00:45:21 -0500

    Add a little header explaining the purpose of phase-01

--- phase-01/main.c
+++ phase-01/main.c
@@ -1,3 +1,8 @@
+/**
+ * Phase 01 - Get a Window that works and can be closed.
+ *
+ * This code won't be structured very well, just trying to get stuff working.
+ */
 #include <stdlib.h>
 
 int main(int argc, char *argv[])

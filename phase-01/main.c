cf45dd1238806c0a9ff74043694f5d276efc7f4a
Faison Zutavern <fzutavern@my.wctc.edu>
2017-07-12 00:47:49 -0500

    Create the display, free it at the end, exit with failure if the display doesn't open

--- phase-01/main.c
+++ phase-01/main.c
@@ -4,8 +4,18 @@
  * This code won't be structured very well, just trying to get stuff working.
  */
 #include <stdlib.h>
+#include <X11/Xlib.h>
 
 int main(int argc, char *argv[])
 {
+  Display *dpy = XOpenDisplay(NULL);
+
+  if (dpy == NULL) {
+    return EXIT_FAILURE;
+  }
+
+  XCloseDisplay(dpy);
+  dpy = NULL;
+
   return EXIT_SUCCESS;
 }

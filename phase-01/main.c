692d1b1581fff4102e88318ca1ead846fa81fd26
Faison Zutavern <fzutavern@my.wctc.edu>
2017-07-12 01:30:24 -0500

    Create, map and destroy the window

--- phase-01/main.c
+++ phase-01/main.c
@@ -8,12 +8,19 @@
 
 int main(int argc, char *argv[])
 {
+  // Create the application window.
   Display *dpy = XOpenDisplay(NULL);
 
   if (dpy == NULL) {
     return EXIT_FAILURE;
   }
 
+  unsigned long black = BlackPixel(dpy, DefaultScreen(dpy));
+  Window win = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 600, 800, 0, black, black);
+  XMapWindow(dpy, win);
+
+  // Free all the things.
+  XDestroyWindow(dpy, win);
   XCloseDisplay(dpy);
   dpy = NULL;
 

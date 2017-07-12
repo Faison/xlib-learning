a517e504a18d0f2f0935efc5c1d5a072ac3e3089
Faison Zutavern <fzutavern@my.wctc.edu>
2017-07-12 01:43:46 -0500

    Setup the Window Manager hints

--- phase-01/main.c
+++ phase-01/main.c
@@ -5,21 +5,36 @@
  */
 #include <stdlib.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 int main(int argc, char *argv[])
 {
-  // Create the application window.
+  // Create application display.
   Display *dpy = XOpenDisplay(NULL);
 
   if (dpy == NULL) {
     return EXIT_FAILURE;
   }
 
+  // Create the application Window.
   unsigned long black = BlackPixel(dpy, DefaultScreen(dpy));
   Window win = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 600, 800, 0, black, black);
+
+  // Setup the Window Manager hints.
+  XWMHints *wmhints = XAllocWMHints();
+  // This basically tells other functions that this contains a value for input and initial state.
+  wmhints->flags = InputHint | StateHint;
+  // And these are the values for input and initial state.
+  wmhints->input = True;
+  wmhints->initial_state = NormalState;
+
+  // Map the window to the display.
   XMapWindow(dpy, win);
 
   // Free all the things.
+  XFree(wmhints);
+  wmhints = NULL;
+
   XDestroyWindow(dpy, win);
   XCloseDisplay(dpy);
   dpy = NULL;

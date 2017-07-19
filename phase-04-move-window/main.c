0010a908f1db60a22a46194175d701dbfd23015b
Faison Zutavern <fzutavern@my.wctc.edu>
2017-07-18 21:49:43 -0500

    Stop being silly and use XMoveWindow()

--- phase-04-move-window/main.c
+++ phase-04-move-window/main.c
@@ -114,19 +114,6 @@ int main(int argc, char *argv[])
   int lookup_buffer_size = 20, charcount = 0;
   Bool chatting = False;
 
-  // Window Move Event.
-  Atom wm_moveresize = XInternAtom(dpy, "_NET_WM_MOVERESIZE", False);
-  XEvent move_window_event;
-  memset(&move_window_event, 0, sizeof(move_window_event));
-  move_window_event.type = ClientMessage;
-  move_window_event.xclient.window = win;
-  move_window_event.xclient.message_type = wm_moveresize;
-  move_window_event.xclient.format = 32;
-  move_window_event.xclient.data.l[0] = pos_x;
-  move_window_event.xclient.data.l[1] = pos_y;
-  move_window_event.xclient.data.l[2] = _NET_WM_MOVERESIZE_MOVE_KEYBOARD;
-  move_window_event.xclient.data.l[4] = 1;
-
   while(!done) {
     // Get the current time.
     clock_gettime(CLOCK_MONOTONIC_RAW, &curr);
@@ -186,14 +173,11 @@ int main(int argc, char *argv[])
             printf("Pressed Ctrl+q, quitting.\n");
             continue;
           } else if (XK_Left == event_key_0) {
-            move_window_event.xclient.data.l[0] -= 10;
-            // ConfigureRequest, maybe???
-            XSendEvent(dpy, DefaultRootWindow(dpy), False, ConfigureRequest, &move_window_event);
-            XFlush(dpy);
+            pos_x -= 50;
+            XMoveWindow(dpy, win, pos_x, pos_y);
           } else if (XK_Right == event_key_0) {
-            move_window_event.xclient.data.l[0] += 10;
-            XSendEvent(dpy, DefaultRootWindow(dpy), False, ConfigureRequest, &move_window_event);
-            XFlush(dpy);
+            pos_x += 50;
+            XMoveWindow(dpy, win, pos_x, pos_y);
           }
 
           if (chatting) {

10f3ee1b3a2756cb380ef415614db322a08dcfc9
Faison Zutavern <fzutavern@my.wctc.edu>
2017-07-19 23:35:56 -0500

    Writing out the plan in a big ol' comment

---
+++ yt-practice-001/main.c
@@ -0,0 +1,9 @@
+/**
+ * Practice of the first video in the YouTubes.
+ *
+ * The goals of the first video are as follows:
+ *   - Compile barebones C program using a Makefile.
+ *   - Open a window, make it display on the screen for a few seconds (using sleep).
+ *   - Make the window stay open until you click the close button.
+ *   - Make sure to clean up after ourselves, shooting for 0 leaks.
+ */
